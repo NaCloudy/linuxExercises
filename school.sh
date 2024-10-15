5ec17beaa2ad4b7cdce02ea6a700a1a30d3d5411
Eva Song <zsong264@wisc.edu>
2024-10-15 17:15:07 -0500

    change awk to group command for sum&avg

--- school.sh
+++ school.sh
@@ -5,5 +5,8 @@
 # pipeline
 cat Property_Tax_Roll.csv | \
 grep "MADISON SCHOOLS" | \
-cut -d',' -f7 | \
-awk 'BEGIN {sum=0; count=0} {sum += $1; count++} END {if (count > 0) print sum/count; else print "No data found."}'
+cut -d ',' -f 7 | \
+{ sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$((count + 1)); done; avg=$(echo "$sum / $count" | bc -l); echo $avg; } 
+    
+# or at the last step, use awk to extract & summarize data
+# awk 'BEGIN {sum=0; count=0} {sum += $1; count++} END {if (count > 0) print sum/count; else print "No data found."}'
