84de9859d02455ca73da96d65b9062072152f72e
Zhengda Liu <zliu2276@wisc.edu>
2024-10-15 14:11:27 -0500

    Complete the five_dirs.sh by Zhengda Liu.

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+rm -rf five
+
+mkdir five
+
+for dir_num in {1..5}; do
+    mkdir five/dir$dir_num
+    
+    for file_num in {1..4}; do
+        seq -s "\n" $file_num | tr -d '[:digit:]' | sed "s/^/$file_num/" > five/dir$dir_num/file$file_num
+    done
+done
+
+echo "Directories and files created successfully."
