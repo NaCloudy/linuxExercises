73cb1950a99228f7a9ad649a67ed27fdfe6da933
Eva Song <zsong264@wisc.edu>
2024-10-15 16:59:50 -0500

    modify rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# remove all files in <dir> larger than <n> bytes 
+
+# usage
+if [[ $# -ne 2 ]]; then
+    echo "usage: ./$0 <dir> <n>"
+    exit 0
+fi
+
+dir=$1
+n=$2
+
+find $dir -type f -size +"${n}c" -exec rm {} \;
