5335341d6af350dcfdb6b7b159a58029b9a5fcd2
ysy <sye78@wisc.edu>
2024-10-15 14:13:20 -0500

    Add school.sh script to calculate average TotalAssessedValue for MADISON SCHOOLS

--- school.sh
+++ school.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+# download Property_Tax_Roll.csv
+# wget http://pages.stat.wisc.edu/~jgillett/DSCP/linux/Property_Tax_Roll.csv
+
+# pipeline
+cat Property_Tax_Roll.csv | \
+grep "MADISON SCHOOLS" | \
+cut -d',' -f7 | \
+awk 'BEGIN {sum=0; count=0} {sum += $1; count++} END {if (count > 0) print sum/count; else print "No data found."}'
