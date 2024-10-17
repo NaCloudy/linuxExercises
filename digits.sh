b9ea83fd925fbbb211820fbd3e81eb6e292d0c50
EdwardSun25 <sxiaoyu2001@gmail.com>
2024-10-17 13:52:16 -0500

    Take sum of numbers from 1000 to 2000 with digits 0 and 1

--- digits.sh
+++ digits.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+sum=0
+#for i in {1000..2000}; do
+for i in {1000..2000}; do
+    pattern="[01][01][01][01]" # match four (0 or 1)s (and maybe other stuff)
+    if [[ $i =~ $pattern ]];then
+	echo $i
+        sum=$(($sum + $i))
+    fi
+done
+echo $sum
+
+#Another intertesting way to do this
+#This idea is actually from Eva Song
+sum2=0
+for i in 1{0..1}{0..1}{0..1};do
+    sum2=$(($sum2 + $i))
+done
+echo $sum2
+
