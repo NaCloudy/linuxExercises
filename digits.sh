3d6068a8704dc97fd35656cb196a74898a832fe2
EdwardSun25 <sxiaoyu2001@gmail.com>
2024-10-17 14:14:43 -0500

    Add a comment to explain the output

--- digits.sh
+++ digits.sh
@@ -8,7 +8,7 @@ for i in {1000..2000}; do
         sum=$(($sum + $i))
     fi
 done
-echo $sum
+echo "Sum of numbers in 1000 to 2000 with only 0 and 1 digits is $sum"
 
 #Another intertesting way to do this
 #This idea is actually from Eva Song
@@ -16,5 +16,5 @@ sum2=0
 for i in 1{0..1}{0..1}{0..1};do
     sum2=$(($sum2 + $i))
 done
-echo $sum2
+echo "This way still provides the same result as before: $sum2"
 
