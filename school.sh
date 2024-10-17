3f8686fce96654c4074e1fbde15b6247e1032966
ysy <sye78@wisc.edu>
2024-10-17 13:30:17 -0500

    ysy

--- school.sh
+++ school.sh
@@ -6,7 +6,22 @@
 cat Property_Tax_Roll.csv | \
 grep "MADISON SCHOOLS" | \
 cut -d ',' -f 7 | \
-{ sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$((count + 1)); done; avg=$(echo "$sum / $count" | bc -l); echo $avg; } 
-    
+{
+    sum=0
+    count=0
+    while read n; do
+        sum=$(($sum + $n))
+        count=$((count + 1))
+    done
+
+    if [ $count -gt 0 ]; then
+        avg=$(echo "$sum / $count" | bc -l)
+        echo "Sum: $sum"
+        echo "Average: $avg"
+    else
+        echo "No data found."
+    fi
+}
+
 # or at the last step, use awk to extract & summarize data
-# awk 'BEGIN {sum=0; count=0} {sum += $1; count++} END {if (count > 0) print sum/count; else print "No data found."}'
+# awk 'BEGIN {sum=0; count=0} {sum += $1; count++} END {if (count > 0) print "Sum: " sum ", Average: " sum/count; else print "No data found."}'
